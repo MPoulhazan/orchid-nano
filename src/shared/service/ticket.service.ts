39ec98720800fe922620b2118a90ec05eab576af
Mickael POULHAZAN <mp041@hotmail.fr>
2022-12-16 16:59:05 -0500

    Task: Add help comments

--- src/shared/service/ticket.service.ts
+++ src/shared/service/ticket.service.ts
@@ -13,7 +13,7 @@ let web3 = new Web3(
             window.location.origin.replace('https', '').replace('http', '')
 );
 
-let lotaddr = '0x6dB8381b2B41b74E17F5D4eB82E8d5b04ddA0a94';
+let lotaddr = '0x6dB8381b2B41b74E17F5D4eB82E8d5b04ddA0a82';
 let lotabi = JSON.parse(
     '[ { "inputs": [ { "internalType": "uint64", "name": "day", "type": "uint64" } ], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "contract IERC20", "name": "token", "type": "address" }, { "indexed": true, "internalType": "address", "name": "funder", "type": "address" }, { "indexed": true, "internalType": "address", "name": "signer", "type": "address" } ], "name": "Create", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "bytes32", "name": "key", "type": "bytes32" }, { "indexed": false, "internalType": "uint256", "name": "unlock_warned", "type": "uint256" } ], "name": "Delete", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "funder", "type": "address" }, { "indexed": true, "internalType": "address", "name": "recipient", "type": "address" } ], "name": "Enroll", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "bytes32", "name": "key", "type": "bytes32" }, { "indexed": false, "internalType": "uint256", "name": "escrow_amount", "type": "uint256" } ], "name": "Update", "type": "event" }, { "inputs": [ { "internalType": "contract IERC20", "name": "token", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "components": [ { "internalType": "bytes32", "name": "data", "type": "bytes32" }, { "internalType": "bytes32", "name": "reveal", "type": "bytes32" }, { "internalType": "uint256", "name": "packed0", "type": "uint256" }, { "internalType": "uint256", "name": "packed1", "type": "uint256" }, { "internalType": "bytes32", "name": "r", "type": "bytes32" }, { "internalType": "bytes32", "name": "s", "type": "bytes32" } ], "internalType": "struct OrchidLottery1.Ticket[]", "name": "tickets", "type": "tuple[]" }, { "internalType": "bytes32[]", "name": "refunds", "type": "bytes32[]" } ], "name": "claim", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "contract IERC20", "name": "token", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "address", "name": "signer", "type": "address" }, { "internalType": "int256", "name": "adjust", "type": "int256" }, { "internalType": "int256", "name": "warn", "type": "int256" }, { "internalType": "uint256", "name": "retrieve", "type": "uint256" } ], "name": "edit", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "signer", "type": "address" }, { "internalType": "int256", "name": "adjust", "type": "int256" }, { "internalType": "int256", "name": "warn", "type": "int256" }, { "internalType": "uint256", "name": "retrieve", "type": "uint256" } ], "name": "edit", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [ { "internalType": "bool", "name": "cancel", "type": "bool" }, { "internalType": "address[]", "name": "recipients", "type": "address[]" } ], "name": "enroll", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "funder", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" } ], "name": "enrolled", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "contract IERC20", "name": "token", "type": "address" }, { "internalType": "address", "name": "signer", "type": "address" }, { "internalType": "uint64", "name": "marked", "type": "uint64" } ], "name": "mark", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" } ], "name": "onTokenTransfer", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "contract IERC20", "name": "token", "type": "address" }, { "internalType": "address", "name": "funder", "type": "address" }, { "internalType": "address", "name": "signer", "type": "address" } ], "name": "read", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "count", "type": "uint256" }, { "internalType": "bytes32", "name": "seed", "type": "bytes32" } ], "name": "save", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "bytes", "name": "data", "type": "bytes" } ], "name": "tokenFallback", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]'
 );
@@ -22,28 +22,6 @@ let token = '0x0000000000000000000000000000000000000000';
 let uint128 = Math.pow(2, 128) - 1;
 let uint64 = Math.pow(2, 64) - 1;
 
-/* import { w3 } from 'web3/auto';
-import * as datetime from 'datetime';
-import * as struct from 'struct';
-import * as json from 'json';
-import * as itertools from 'itertools';
-import * as math from 'math';
-
-let gnosisrpc = 'https://rpc.gnosischain.com/';
-let w3 = new web3.Web3(new web3.HTTPProvider(gnosisrpc));
-
-
-let addrtype = Math.pow(2, 20 * 8) - 1;
-
-
-// let token = "0x" + "0" * 40; // TODO
-
-// TODO
-function to_32byte_hex(val: any) {
-    return val;
-}
-*/
-// TODO
 function ticket(
     amount: any,
     reveal: any,
@@ -114,6 +92,7 @@ function ticket(
     return tk;
 }
 /*
+//TODO: Finish function
 function claimTicket(tkt: any, plyr: any) {
     var func, l1nonce, signed, tx, txhash;
     l1nonce = w3.eth.get_transaction_count(plyr['account']);
@@ -132,6 +111,7 @@ function claimTicket(tkt: any, plyr: any) {
     console.log('Claim transaction hash: ', txhash.hex());
 }
 
+//TODO: Finish function
 function serializeTicket(tk: any) {
     return (
         tk[0].hex() +
@@ -144,6 +124,7 @@ function serializeTicket(tk: any) {
     return json.dumps([tk[0].hex(), tk[1].hex(), tk[2], tk[3], tk[4], tk[5]]);
 }
 
+//TODO: Finish function
 function deserializeTicket(tstr: any) {
     let tk;
 
@@ -188,8 +169,8 @@ export const newReveal = (username: any) => {
     };
 };
 /*
-var _pj;
 
+//TODO: Finish function
 function _pj_snippets(container) {
     function in_es6(left, right) {
         if (right instanceof Array || typeof right === 'string') {
@@ -212,10 +193,7 @@ function _pj_snippets(container) {
     return container;
 }
 
-_pj = {};
-
-_pj_snippets(_pj);
-
+//TODO: Finish function
 function printPlayer(plyr: any) {
     var account, balance, escrow;
     account = plyr['account'];
@@ -292,7 +270,9 @@ export const generateTicket = (plyr: Player, oppo: Player) => {
     );
     console.log(tk);
 };
+
 /*
+//TODO: Finish function
 function isWinner(tk: any) {
     var comp, data, hash, nonce, packed0, packed1, r, ratio, reveal, s;
     [data, reveal, packed0, packed1, r, s] = tk;
@@ -311,6 +291,7 @@ function isWinner(tk: any) {
     return true;
 }
 
+//TODO: Finish function
 function printTicket(tk: any) {
     var amount, data, funder, nonce, packed0, packed1, r, ratio, reveal, s;
     [data, reveal, packed0, packed1, r, s] = tk;
@@ -337,6 +318,7 @@ function printTicket(tk: any) {
     }
 }
 
+//TODO: Finish function
 function claim(plyr: any, oppo: any) {
     var tk, tstr;
 
@@ -356,6 +338,7 @@ function claim(plyr: any, oppo: any) {
     }
 }
 
+//TODO: Finish function
 function printInfo(plyr: any, oppo: any) {
     console.log('Your information:');
     printPlayer(plyr);
